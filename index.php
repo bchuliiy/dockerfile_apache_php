418f622adbc335c3d261da9a980df732213a0569
og <rmblviv@gmail.com>
2022-05-02 13:32:52 +0000

    commit1

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello from k8s</h1><br>";
+echo "Server IP Addres is: $ip_server", "<br><p>";
+echo "Made by <font color=blue>bchuliy";
+
+?>
